2b48551097506e2373efa95f0c3cb86feecef0fb
rrodrig6 <rrodrig6@uccs.edu>
2022-02-13 16:21:17 -0700

    Move GameObject behaviors to separate scripts

--- gameengine/GameObject.py
+++ gameengine/GameObject.py
@@ -1,6 +1,8 @@
 import pygame, yaml
 from pygame.math import Vector2
 
+from bounce_box_behavior import *
+
 class GameObject(yaml.YAMLObject, pygame.sprite.Sprite):
     yaml_loader = yaml.SafeLoader
     yaml_tag = u'!GameObject'
@@ -10,49 +12,35 @@ class GameObject(yaml.YAMLObject, pygame.sprite.Sprite):
         return GameObject(**loader.construct_mapping(node))
 
 
-    def __init__(self, x: int=0, y: int=0, components = {} ):
+    def __init__(self, x: int=0, y: int=0, components = {}, behaviors = {} ):
         super().__init__()
         print('GameObject __init__()')
         # Motion Setup
         self.position = Vector2(x,y)
         self.image = pygame.Surface((0,0))
         self.components = components
+        self.behaviors = { BounceBoxBehavior() }
         self.rect = pygame.Rect(self.position.x, self.position.y,0,0)
         self.speed = [4,4]
 
     def component_setup(self):
         for component in self.components:
             component.parent = self
+        for behavior in self.behaviors:
+            behavior.parent = self
         
     def attach_component(self, component) -> None:
         self.components[component.name] = component
 
-    def do_border_collision(self, game):
-
-        if self.rect.left < 0 or self.rect.right > game.screen_width:
-           self.speed[0] = -self.speed[0]
-
-        if self.rect.top < 0 or self.rect.bottom > game.screen_height:
-           self.speed[1] = -self.speed[1]
-
-    def do_sprite_collision(self, collision_group):
-        collision_list = pygame.sprite.spritecollide(self, collision_group, False)
-        for i in collision_list:
-            if i != self:
-                if abs(self.rect.y-i.rect.y) < abs(self.rect.x-i.rect.x):
-                    self.speed[0] = -self.speed[0]
-                else:
-                    self.speed[1] = -self.speed[1]
 
     def update(self, *args, **kwargs):
         super().update(*args, **kwargs)
 
         for component in self.components:
             component.update()
-            #component.rect.x = self.rect.x
-            #component.rect.y = self.rect.y
+            
+        for behavior in self.behaviors:
+            behavior.update(*args, **kwargs)
         
-        self.rect.move_ip(self.speed)
-        self.do_border_collision(kwargs['game'])
-        self.do_sprite_collision(kwargs["collision_group"])
+        
         
---
+++ gameengine/bounce_box_behavior.py
@@ -0,0 +1,28 @@
+import pygame
+
+class BounceBoxBehavior():
+    def __init__(self):
+        self.parent = None
+
+    def update(self, *args, **kwargs):
+        self.parent.rect.move_ip(self.parent.speed)
+        self.do_border_collision(kwargs['game'])
+        self.do_sprite_collision(kwargs["collision_group"])
+
+
+    def do_border_collision(self, game):
+
+        if self.parent.rect.left < 0 or self.parent.rect.right > game.screen_width:
+           self.parent.speed[0] = -self.parent.speed[0]
+
+        if self.parent.rect.top < 0 or self.parent.rect.bottom > game.screen_height:
+           self.parent.speed[1] = -self.parent.speed[1]
+
+    def do_sprite_collision(self, collision_group):
+        collision_list = pygame.sprite.spritecollide(self.parent, collision_group, False)
+        for i in collision_list:
+            if i != self:
+                if abs(self.parent.rect.y-i.rect.y) < abs(self.parent.rect.x-i.rect.x):
+                    self.parent.speed[0] = -self.parent.speed[0]
+                else:
+                    self.parent.speed[1] = -self.parent.speed[1]
